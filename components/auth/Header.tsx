95716def907448e89cb4c551b8d36f73e93e70a5
tobias-barakaa <ngairato@gmail.com>
2025-02-26 12:48:42 +0300

    trying my best

--- components/auth/Header.tsx
+++ components/auth/Header.tsx
@@ -16,12 +16,12 @@ const Header = ({ label }: HeaderProps) => {
     <div className="w-full flex flex-col gap-y-4 items-center justify-center">
       <div className="flex items-center gap-3 group">
         <div className="relative transition-transform duration-300 group-hover:scale-110">
-          <div className="absolute inset-0 bg-gradient-to-r  from-[#f059da]/30 to-[#f059da]/30 rounded-full" />
+          <div className="absolute inset-0 bg-gradient-to-r from-[#f059da]/30 to-[#a03494]/30 rounded-full" />
           <Image 
             src="/igleadlogo.png" 
             alt="" 
-            width={50} 
-            height={50}
+            width={70} 
+            height={70}
             className="object-contain relative z-10 drop-shadow-[0_0_8px_rgba(240,89,218,0.5)]"
           />
         </div>
