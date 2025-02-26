c45b3736a7af224bfc39a46edb24f66a04c3cf5f
tobias-barakaa <ngairato@gmail.com>
2025-02-26 12:14:46 +0300

    debug

--- components/common/Logo.tsx
+++ components/common/Logo.tsx
@@ -16,13 +16,13 @@ const Header = ({ label }: HeaderProps) => {
     <div className="w-full flex flex-col items-center justify-center gap-2">
       <div className="flex items-center gap-3 group">
         <div className="relative transition-transform duration-300 group-hover:scale-110">
-          <div className="absolute inset-0  rounded-full blur-md" />
+          <div className="absolute inset-0 bg-gradient-to-r from-[#f059da]/30 to-[#a03494]/30 rounded-full" />
           <Image 
             src="/igleadlogo.png" 
             alt="Logo" 
-            width={20} 
-            height={30}
-            className="object-contain relative z-10"
+            width={50} 
+            height={50}
+            className="object-contain relative z-10 drop-shadow-[0_0_8px_rgba(240,89,218,0.5)]"
           />
         </div>
         <h1 className={cn(
