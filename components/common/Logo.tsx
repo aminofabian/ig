48323e294c5d00b9a46686e8860afba2f0503560
tobias-barakaa <ngairato@gmail.com>
2025-02-26 12:45:19 +0300

    debugging

--- components/common/Logo.tsx
+++ components/common/Logo.tsx
@@ -19,7 +19,7 @@ const Header = ({ label }: HeaderProps) => {
           <div className="absolute inset-0 bg-gradient-to-r from-[#f059da]/30 to-[#a03494]/30 rounded-full blur-md" />
           <Image 
             src="/instagram-icon.png" 
-            alt="Logo" 
+            alt="" 
             width={50} 
             height={50}
             className="object-contain relative z-10 drop-shadow-[0_0_8px_rgba(240,89,218,0.5)]"
